319d8520c04816c3e9e8d1a26ac4a84a238bbcda
Florian Rohrer <semicolonator@gmail.com>
2024-12-01 10:08:33 +0100

    Day 1

--- day01/main.py
+++ day01/main.py
@@ -1,20 +1,12 @@
 from collections import Counter
-from heapq import heapify, heappop
 
 with open("input.txt") as f:
     lines = [[int(i) for i in l.split()] for l in f]
 
 # Part 1
-l1, l2 = [list(x) for x in zip(*lines)]
-heapify(l1)
-heapify(l2)
-
-res = 0
-while l1:
-    res += abs(heappop(l1)-heappop(l2))
-print(res)
+l1, l2 = [list(sorted(x)) for x in zip(*lines)]
+print(sum(abs(a-b) for a, b in zip(l1, l2)))
 
 # Part 2
-l1, l2 = zip(*lines)
 counter = Counter(l2)
 print(sum(i*counter[i] for i in l1))
