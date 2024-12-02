c5244aca1e15ee7fb6b6633b811723ba11314a1a
Florian Rohrer <semicolonator@gmail.com>
2024-12-02 09:22:14 +0100

    day1

--- day01/main.py
+++ day01/main.py
@@ -4,7 +4,7 @@ with open("input.txt") as f:
     lines = [[int(i) for i in l.split()] for l in f]
 
 # Part 1
-l1, l2 = [list(sorted(x)) for x in zip(*lines)]
+l1, l2 = [sorted(x) for x in zip(*lines)]
 print(sum(abs(a-b) for a, b in zip(l1, l2)))
 
 # Part 2
